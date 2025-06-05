2e0e5ef78dd84c176e16979a1ce06bb0767f12c5
DELL <DELL@192.168.0.112>
2025-06-05 11:18:03 +0530

    Add new field called rollNo in Details

--- Student/src/com/org/Details.java
+++ Student/src/com/org/Details.java
@@ -5,9 +5,10 @@ public class Details {
 	public static void main(String[] args) {
 		String name = "abhi";
 		int age = 27;
-
+		int rollNo = 123;
 		System.out.println(name);
 		System.out.println(age);
+		System.out.println(rollNo);
 	}
 
 }
